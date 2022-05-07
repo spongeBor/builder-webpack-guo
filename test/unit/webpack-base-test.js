303801e49fd0e77990ee0bed1c961405534ae123
bigBuzy <guoyubo00737@163.com>
2022-05-07 17:36:08 +0800

    test: webpack.base test

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 describe('webpack.base.js test case', () => {
   const baseConfig = require('../../lib/webpack.base');
   it('entry', () => {
-    assert.equal(baseConfig.entry.index, '/Users/guoyubo/Desktop/my-project-webpack/builder-webpack/test/smoke/template/src/index/index.js') 
-    assert.equal(baseConfig.entry.search, '/Users/guoyubo/Desktop/my-project-webpack/builder-webpack/test/smoke/template/src/search/index.js')
+    assert.equal(baseConfig.entry.index.indexOf('builder-webpack-guo/test/smoke/template/src/index/index.js') > -1, true); 
+    assert.equal(baseConfig.entry.search.indexOf('builder-webpack-guo/test/smoke/template/src/search/index.js') > -1, true)
   })
 })
